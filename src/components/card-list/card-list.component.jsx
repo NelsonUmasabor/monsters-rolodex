d02f1f86748635dd041c8042e3d0c1b2111c663c
NelsonUmasabor <umasabornelson@gmail.com>
2022-03-22 17:26:57 +0100

    new update

--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -5,8 +5,8 @@ import './card-list.styles.css';
 export const CardList = props => (   
     <div className = 'card-list'> 
         {props.monsters.map(monster =>(
-            <Card key = {monster.id} monster = {monster} /> 
+        <Card key = {monster.id} monster = {monster} /> 
         ))
-        }; 
+        }
     </div>
 );
